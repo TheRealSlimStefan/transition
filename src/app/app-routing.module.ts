b3a31314073ee9e3962d7f4d5c54aeb4f75c9149
TheRealSlimStefan <66061781+TheRealSlimStefan@users.noreply.github.com>
2022-05-17 12:55:11 +0200

    another commit 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { AddUserComponent } from './add-user/add-user.component';
+import { ModifyUserComponent } from './modify-user/modify-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { ListUsersComponent } from './list-users/list-users.component';
 
 const routes: Routes = [
-  {path: 'add-user', component: AddUserComponent}
+  {path: 'add-user', component: AddUserComponent},
+  {path: 'modify-user', component: ModifyUserComponent}, 
+  {path: 'delete-user', component: DeleteUserComponent}, 
+  {path: 'list-users', component: ListUsersComponent}
 ]
 
 @NgModule({
