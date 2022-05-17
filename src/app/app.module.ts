9bfb75ba6edd0dcff52ddc8b815b55f0535d3e8b
TheRealSlimStefan <66061781+TheRealSlimStefan@users.noreply.github.com>
2022-05-17 12:35:19 +0200

    another commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './add-user/add-user.component';
+import { ModifyUserComponent } from './modify-user/modify-user.component';
+import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { ListUsersComponent } from './list-users/list-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    ModifyUserComponent,
+    DeleteUserComponent,
+    ListUsersComponent
   ],
   imports: [
     BrowserModule
